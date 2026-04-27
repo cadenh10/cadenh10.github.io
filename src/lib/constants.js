fb525a8724ca6ba7e334136a85565634c64bb6f3
cadenh10 <cadenleeho@gmail.com>
2026-04-26 21:24:05 -0400

    Updated download button to correct directory

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
 export const DOWNLOAD_URL =
-  "https://github.com/cadenh10/Dialed/releases/latest/download/dialed-mac.dmg";
+  "https://github.com/cadenh10/dialed-updates/raw/main/dialed-mac.dmg";
 
 export const SUPPORT_EMAIL = "workdialed@gmail.com";
